49c027036fea6b61a8e821bff03ece5f6c1de33c
Isaac Joshua <isaacjcarnes@gmail.com>
2021-11-29 23:29:21 -0500

    Added delete functions and README file

--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO department(name)
-VALUES ("Management"),
-        ("Research"),
-        ("Development"),
-        ("Marketing");
-
-INSERT INTO role(title, salary, department_id)
-VALUES 
--- index.js
+++ index.js
@@ -20,12 +20,16 @@ const mainMenuChoices = [
   "View All Employees",
   "Add Employee",
   "Update Employee Role",
+  "Delete Employee",
   "View All Roles",
   "Add Role",
+  "Delete Role",
   "View All Departments",
   "Add Department",
+  "Delete Department",
   "Quit",
 ];
+
 function mainMenu() {
   inquirer
     .prompt([
@@ -42,6 +46,9 @@ function mainMenu() {
           mainMenuChoices[5],
           mainMenuChoices[6],
           mainMenuChoices[7],
+          mainMenuChoices[8],
+          mainMenuChoices[9],
+          mainMenuChoices[10],
         ],
       },
     ])
@@ -57,27 +64,36 @@ function mainMenu() {
           updateEmpRole();
           break;
         case mainMenuChoices[3]:
-          viewRoles();
+          deleteEmployee();
           break;
         case mainMenuChoices[4]:
-          addRole();
+          viewRoles();
           break;
         case mainMenuChoices[5]:
-          viewDepartments();
+          addRole();
           break;
         case mainMenuChoices[6]:
-          addDepartment();
+          deleteRole();
           break;
         case mainMenuChoices[7]:
+          viewDepartments();
+          break;
+        case mainMenuChoices[8]:
+          addDepartment();
+          break;
+        case mainMenuChoices[9]:
+          deleteDepartment();
+          break;
+        case mainMenuChoices[10]:
           console.log("Exiting application, goodbye.");
           process.exit();
       }
     });
 }
 
-function viewEmployees() { //Queries all employees and logs them in table //WORK TODO
+function viewEmployees() { //Queries all employees and logs them in table //Only shows employees with managers currently
   db.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title , department.name AS department, role.salary FROM employee INNER JOIN role ON employee.role_id=role.id INNER JOIN department ON role.department_id=department.id",
+    "SELECT emp.id, emp.first_name, emp.last_name, role.title , department.name AS department, role.salary, CONCAT(man.first_name, ' ', man.last_name) AS manager FROM employee AS emp INNER JOIN role ON emp.role_id=role.id INNER JOIN department ON role.department_id=department.id LEFT JOIN employee AS man ON emp.manager_id = man.id",
     function (err, results) {
       if (err) {
         console.log(err);
@@ -144,9 +160,8 @@ function addEmployee() { //Asks for relevant data and then adds new employee to 
           ])
           .then((empData) => {
             let managerId = empData.emp_manager;
-            console.log(managerId);
             if (managerId.id) {
-              managerId = managerID.id;
+              managerId = managerId.id;
             } else {
               managerId = null;
             }
@@ -178,7 +193,7 @@ function addEmployee() { //Asks for relevant data and then adds new employee to 
   });
 }
 
-function updateEmpRole(){
+function updateEmpRole(){ //Asks for relevant data and then changes employee's role to new role
   let currentEmployees = "";
   let currentRoles = "";
   db.query("SELECT first_name, last_name, id FROM employee;", function (err, result) {
@@ -196,7 +211,6 @@ function updateEmpRole(){
       function (err, result) {
         if (err) {
         }
-        console.log(result);
         currentRoles = result.map((role) => ({
           name: role.title,
           value: { id: role.id, name: role.title },
@@ -220,7 +234,7 @@ function updateEmpRole(){
       ]).then((empData) => {
         let empId = empData.emp.id;
         let empRole = empData.emp_role.id;
-        db.query("UPDATE employee SET role_id = ? WHERE id = ?;", [empId, empRole], function(err, result){
+        db.query("UPDATE employee SET role_id = ? WHERE id = ?;", [empRole, empId], function(err, result){
           if(err){
             console.log(err);
           }
@@ -232,9 +246,43 @@ function updateEmpRole(){
   });
 }
 
-function viewRoles() { //Queries all roles and logs them in table //WORK TODO
+function deleteEmployee(){ //Asks for employee from list and removes employee from employee table
+  let currentEmployees = "";
+  db.query("SELECT first_name, last_name, id FROM employee;", function (err, result) {
+    if (err) {
+    }
+    currentEmployees = result.map((emp) => ({
+      name: emp.first_name + " " + emp.last_name,
+      value: {
+        id: emp.id,
+        name: emp.first_name + " " + emp.last_name,
+      },
+    }));
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which employee do you want to delete?",
+        name: "emp",
+        choices: currentEmployees,
+      },
+    ]).then((empData) => {
+      let fName = empData.emp.first_name;
+      let lName = empData.emp.last_name;
+      let empId = empData.emp.id;
+      db.query("DELETE FROM employee WHERE id = ?", empId, function(err, result){
+        if(err){
+          console.log(err);
+        }
+        console.log("Deleted " + fName + " " + lName + " from employee table.");
+        mainMenu();
+      });
+    });
+  });
+}
+
+function viewRoles() { //Queries all roles and logs them in table
   db.query(
-    "SELECT role.id, role.title, role.salary FROM role", //Needs department name
+    "SELECT role.title, role.id, department.name AS department, role.salary FROM role INNER JOIN department ON role.department_id=department.id",
     function (err, results) {
       if (err) {
         console.log(err);
@@ -294,6 +342,36 @@ function addRole() { //Asks for relevant data and then adds new role to role tab
   });
 }
 
+function deleteRole() { //Asks for role from list and removes role from role table
+  let currentRoles = "";
+  db.query("SELECT title, id FROM role;", function (err, result) {
+    if (err) {
+    }
+    currentRoles = result.map((role) => ({
+      name: role.title,
+      value: { id: role.id, name: role.title },
+    }));
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which role do you want to delete?",
+        name: "role",
+        choices: currentRoles,
+      },
+    ]).then((roleData) => {
+      let roleTitle = roleData.role.title;
+      let roleId = roleData.role.id;
+      db.query("DELETE FROM role WHERE id = ?", roleId, function(err, result){
+        if(err){
+          console.log(err);
+        }
+        console.log("Deleted " + roleTitle + " from role table.");
+        mainMenu();
+      });
+    });
+  });
+}
+
 function viewDepartments() { //Queries all departments and logs them in table
   db.query(
     "SELECT department.id, department.name FROM department",
@@ -329,6 +407,36 @@ function addDepartment() { //Asks for relevant data and then adds new department
         }
       );
     });
+}
+
+function deleteDepartment(){ //Asks for department from list and removes derpartment from derpartment table
+  let currentDepartments = "";
+  db.query("SELECT name, id FROM department;", function (err, result) {
+    if (err) {
+    }
+    currentDepartments = result.map((department) => ({
+      name: department.name,
+      value: { id: department.id, name: department.name },
+    }));
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which department do you want to delete?",
+        name: "department",
+        choices: currentDepartments,
+      },
+    ]).then((depData) => {
+      let depName = depData.department.name;
+      let depId = depData.department.id;
+      db.query("DELETE FROM department WHERE id = ?", depId, function(err, result){
+        if(err){
+          console.log(err);
+        }
+        console.log("Deleted " + depName + " from department table.");
+        mainMenu();
+      });
+    });
+  });
 }
 
 //Starts the inquirer loop
